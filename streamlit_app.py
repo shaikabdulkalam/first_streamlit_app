1bcb1a272085cd2e25fc45cc25714c2e621363d1
shaikabdulkalam <112329166+shaikabdulkalam@users.noreply.github.com>
2024-01-31 15:07:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
 streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
